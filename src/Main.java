20f71310b9cf496d6d44752d9d737daf20bf5803
Victoria Riso Santana <devvicrisosan@gmail.com>
2026-03-30 11:45:31 -0300

    feat(bemVindos): add entrada de nome1, nome2 e tratamento de erro para String vazia

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         byte[][] tabuleiro = new byte[3][3];
+        bemVindos();
         exibirTabuleiro(tabuleiro);
+    }
+
+    private static String[] bemVindos(){
+        System.out.print("Bem vindos! ao:\n----------JOGO-DA-VELHA-EM-JAVA----------\n");
+        String nome1, nome2;
+        try {
+            do {
+                System.out.println("Digite o nome do primeiro jogador(X):");
+                nome1 = sc.nextLine();
+
+                String erro = nome1.isBlank() ? "Nome não pode ser vázio. Tente novamente:" : ""; //isBlank() verifica se está vazia ou com caracteres de espaço
+                System.out.println(erro);
+            } while(nome1.isBlank());
+
+            do {
+                System.out.println("Digite o nome do segundo jogador(0):");
+                nome2 = sc.nextLine();
+
+                String erro = nome2.isBlank() ? "Nome não pode ser vázio. Tente novamente:" : "";
+                System.out.println(erro);
+            } while(nome2.isBlank());
+        } finally {
+            sc.close();
+        }
+        return new String[]{nome1, nome2};
     }
 
     private static void exibirTabuleiro(byte[][] tabuleiro){
