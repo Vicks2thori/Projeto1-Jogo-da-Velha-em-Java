59c5e3859fa451bbf07427b92b4a06dfde590c2d
Victoria Riso Santana <devvicrisosan@gmail.com>
2026-03-30 11:07:04 -0300

    feat(exibirTabuleiro): add simbolo e exibe a matriz

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        byte[][] tabuleiro = new byte[3][3];
+        exibirTabuleiro(tabuleiro);
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    private static void exibirTabuleiro(byte[][] tabuleiro){
+        for (byte[] linha : tabuleiro) {
+            for (byte valor : linha) {
+                char simbolo = valor == 1 ? 'X' : valor == 2 ? 'O' : ' ';
+                System.out.printf("[%c]", simbolo);
+            }
+            System.out.println();
         }
     }
+
 }
